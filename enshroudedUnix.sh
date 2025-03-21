52328efd20a1e21b57d0ab746ed272abbbe22c33
Ethan Park <70242462+ethapar@users.noreply.github.com>
2025-03-21 11:42:54 -0400

    Add files via upload

---
+++ enshroudedUnix.sh
@@ -0,0 +1,50 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+sudo apt update && sudo apt upgrade -y 
+sudo apt install unzip apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
+sudo apt update
+
+#install AWS CLI
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
+sudo ./aws/install
+
+#create random string for password
+ENPW=$(echo $RANDOM | md5sum | head -c 20)
+
+#get stackname created by user data script and update SSM parameter name with this to make it unique
+STACKNAME=$(</tmp/enshParamName.txt)
+PARAMNAME=enshPW-$STACKNAME
+
+#put random string into parameter store as encrypted string value
+aws ssm put-parameter --name $PARAMNAME --value $ENPW --type "SecureString" --overwrite
+
+
+#install docker and valheim app on docker
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo apt install docker-compose -y
+sudo usermod -aG docker $USER
+sudo mkdir /usr/games/serverconfig
+cd /usr/games/serverconfig
+sudo bash -c 'echo "version: \"3\"
+services:
+  enshrouded:
+    image: mornedhels/enshrouded-server:latest
+    container_name: enshrouded
+    hostname: enshrouded
+    restart: unless-stopped
+    stop_grace_period: 90s
+    ports:
+      - "15637:15637/udp"
+    volumes:
+      - ./game:/opt/enshrouded
+    environment:
+      - SERVER_NAME=Enshrouded Server
+      - SERVER_PASSWORD=secret
+      - UPDATE_CRON=*/30 * * * *
+      - PUID=4711
+      - PGID=4711'
+echo "@reboot root (cd /usr/games/serverconfig/ && docker-compose up)" > /etc/cron.d/awsgameserver
+sudo docker-compose up
